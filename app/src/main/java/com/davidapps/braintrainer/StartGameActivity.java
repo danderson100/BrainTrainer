dbf6812ce535bb3289ded35114f42031d5dde9b3
David Anderson <danderson7790@gmail.com>
2021-04-12 09:12:23 -0700

    four buttons in table layout properly formatted

--- app/src/main/java/com/davidapps/braintrainer/StartGameActivity.java
+++ app/src/main/java/com/davidapps/braintrainer/StartGameActivity.java
@@ -1,4 +1,14 @@
 package com.davidapps.braintrainer;
 
-public class StartGameActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+    }
 }
